aa26aa0a495495b3f3d3a1c1d6135104d7f7aae9
Jon Parise <jon@pinterest.com>
2016-07-11 11:37:51 -0700

    Add thrift-gen for linting generated Thrift files
    Lints files generated by the Apache Thrift compiler to ensure they were
    generated using a supported Thrift compiler.

--- __phutil_library_map__.php
+++ __phutil_library_map__.php
@@ -9,6 +9,7 @@
 phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
+    'ApacheThriftGeneratedLinter' => 'src/ApacheThriftGeneratedLinter.php',
     'ApacheThriftLinter' => 'src/ApacheThriftLinter.php',
     'GoVetLinter' => 'src/GoVetLinter.php',
     'PythonImportsLinter' => 'src/PythonImportsLinter.php',
@@ -16,6 +17,7 @@ phutil_register_library_map(array(
   ),
   'function' => array(),
   'xmap' => array(
+    'ApacheThriftGeneratedLinter' => 'ArcanistLinter',
     'ApacheThriftLinter' => 'ArcanistExternalLinter',
     'GoVetLinter' => 'ArcanistExternalLinter',
     'PythonImportsLinter' => 'ArcanistLinter',
